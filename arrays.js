6776f33a470e5730af5f979749d22d6852278269
Jorge Obrecht <jorgeobrecht@gmail.com>
2021-07-11 13:31:19 -0300

    Arrays, Spread & Métodos

---
+++ arrays.js
@@ -0,0 +1,57 @@
+const alunosGama=["Jorge","Jose","Josue","Joao"]
+
+//Acessar informacoes no Array
+console.log(alunosGama[[3]])
+
+//Operador spread(...)
+const alunesGama=[...alunosGama,"Janaina"]
+
+console.log(alunesGama)
+
+//metodos de iteracao
+
+/* for (let i=0 ; i<alunesGama.length ;i++){
+    console.log(alunesGama[i])
+} */
+
+//Map
+alunesGama.map(alune=>console.log(alune))
+
+//Filter
+const numeros = [34, 45, 67, 90, 55, 76]
+
+const numerosImpares = numeros.filter(numero => numero%2 !=0)
+console.log(numerosImpares)
+
+const numerosPares = numeros.filter(numero => numero%2 ==0)
+console.log(numerosPares)
+
+
+const produtos = ["geladeira", "fogao", "cama", "mesa"]
+
+//find
+
+const encontraCama = produtos.find(produto => produto === "cama")
+console.log(encontraCama)
+
+const encontraMesa = produtos.find(produto => produto === "mesa")
+console.log(encontraMesa)
+
+//sort - ordenação 
+
+const numerosOrdenadosCrescente = numeros.sort()
+console.log(numerosOrdenadosCrescente)
+
+const numerosOrdenadosDecrescente = numeros.sort((a,b)=> b-a)
+console.log(numerosOrdenadosDecrescente)
+
+
+//reduce - reduz nosso array a um resultado de uma operação matemática
+
+const numbers = [1,34,35]
+
+const soma = numbers.reduce((valorAnterior, valorAtual)=> {
+    return valorAnterior + valorAtual
+},5)
+
+console.log(soma) //75
